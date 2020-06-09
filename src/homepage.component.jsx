cb3f88c9605b6204292860e4ddd0438f327a7131
unknown <cezheng@buffalo.edu>
2020-06-09 17:08:48 -0400

    create store project

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './homepage.styles.scss'
+export const HomePage = ()=>(
+     <div className='homepage'>
+         <div className='directort-menu'>
+             <div className='menu-item'>
+                 <div className='content'>
+                     <h1 className='title'>Hats</h1>
+                     <span className='subtitle'>Shop Now</span>
+                 </div>
+             </div>
+             <div className='menu-item'>
+                 <div className='content'>
+                     <h1 className='title'>Jackets</h1>
+                     <span className='subtitle'>Shop Now</span>
+                 </div>
+             </div>
+             <div className='menu-item'>
+                 <div className='content'>
+                     <h1 className='title'>Showes</h1>
+                     <span className='subtitle'>Shop Now</span>
+                 </div>
+             </div>
+             <div className='menu-item'>
+                 <div className='content'>
+                     <h1 className='title'>Women</h1>
+                     <span className='subtitle'>Shop Now</span>
+                 </div>
+             </div>
+             <div className='menu-item'>
+                 <div className='content'>
+                     <h1 className='title'>Men</h1>
+                     <span className='subtitle'>Shop Now</span>
+                 </div>
+             </div>
+         </div>
+     </div>
+ )
+
+//  export default HomePage
